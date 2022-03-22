72777a1f6157d59ee0d324dc51a53875fd2398dd
andrelimaiftm <andrelima@iftm.edu.br>
2022-03-22 20:54:19 -0300

    alteração da classe Exemplo e criação da classe Exemplo2

--- Exemplo.java
+++ Exemplo.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Exemplo {
     public static void main(String[] args) {
         System.out.println("Olá git e github!!!");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite um valor: ");
+        int valor1 = entrada.nextInt();
+
+        System.out.print("Digite um outro valor:");
+        int valor2 = entrada.nextInt();
+
+        int soma = valor1 + valor2;
+
+        System.out.println("Resultado: " + soma);
+
+        entrada.close();        
     }
 }
